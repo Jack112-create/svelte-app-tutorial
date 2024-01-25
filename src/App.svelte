c7da59e8fd6fc0eaffa6012299bd13d9b6b7dc2c
Jack112-create <jackconroy777@gmail.com>
2024-01-25 14:54:12 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Modal from './Modal.svelte';
+
+	let people = [
+		{name: "Jack", beltColour: "black", age: 23, id: 1},
+		{name: "Luke", beltColour: "orange", age: 23, id: 2},
+		{name: "Conal", beltColour: "purple", age: 26, id: 3}
+	];
+
+	const handleClick = (id) => {
+		people = people.filter((person) => id != person.id);
+	}
+</script>
+
+<Modal message="Hey, I am a prop value" isPromo={true} />
+
+<main>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColour === "black"}
+				<p><strong>Master ninja</strong></p>
+			{/if}
+			<p>{person.age} years old</p>
+			<p>{person.beltColour} belt</p>
+			<button on:click={() => handleClick(person.id)}>delete</button>
+		</div>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Modal.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let message;
+  let shoModal = true;
+  export let isPromo = false;
+</script>
+
+{#if shoModal}
+  <div class="backdrop" class:promo={isPromo}>
+    <div class="modal">
+      <p>{message}</p>
+    </div>
+  </div>
+{/if}
+
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.8);
+  }
+
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background: #fff;
+  }
+
+  .promo .modal {
+    background: crimson;
+    color: #fff;
+  }
+</style>
